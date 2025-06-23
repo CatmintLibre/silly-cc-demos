08c926260a14fb4c67afe83e8e61321bba60dd8a
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2025-06-23 22:36:35 +0200

    Update intruderAlert.lua

--- demos/intruderAlert.lua
+++ demos/intruderAlert.lua
@@ -13,4 +13,4 @@ mon setTextColor(colors.red)
 music:play()
 
 ---Write text on Monitor
-mf.writeOn(mon,  "Wellcome to Aether Industries HQ", nil, nil, {font = "fonts/Silkscreen", textAlign = "center", condense = true})
+mf.writeOn(mon,  "!!!INTRUDER DETECTED!!!", nil, nil, {font = "fonts/Silkscreen", textAlign = "center", condense = true})
