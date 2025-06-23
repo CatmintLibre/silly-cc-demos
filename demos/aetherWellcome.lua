86abea6464070927e7b8da8fe152c61d88591f21
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2025-06-23 21:52:49 +0200

    Update aetherWellcome.lua

--- demos/aetherWellcome.lua
+++ demos/aetherWellcome.lua
@@ -1,1 +1,9 @@
+local mf = require("morefonts") ---requires https://pinestore.cc/projects/37/more-fonts
 
+---Define monitor, clear it and define text color
+mon = peripheral.wrap("monitor_4")
+mon.clear()
+mon setTextColor(colors.lightBlue)
+
+---Write text on Monitor
+mf.writeOn(mon,  "Wellcome to Aether Industries HQ", nil, nil)
