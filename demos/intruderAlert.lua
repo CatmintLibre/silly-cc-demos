607622bbe9e1a6290c20d90561b1a6196fa746b2
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2025-06-23 22:35:44 +0200

    Create intruderAlert.lua

---
+++ demos/intruderAlert.lua
@@ -0,0 +1,16 @@
+local mf = require("morefonts") ---requires https://pinestore.cc/projects/37/more-fonts
+local nbsTunes = require("nbsTunes") -- requires https://pinestore.cc/projects/18/nbs-tunes
+
+local sound = nbsTunes.load("../tunes/redAlarm.nbs")
+
+---Define monitor, clear it and define text color
+mon = peripheral.wrap("monitor_4")
+sound:stop()
+mon.clear()
+mon setTextColor(colors.red)
+
+-- Play the music
+music:play()
+
+---Write text on Monitor
+mf.writeOn(mon,  "Wellcome to Aether Industries HQ", nil, nil, {font = "fonts/Silkscreen", textAlign = "center", condense = true})
