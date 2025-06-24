c9321322c7aae3cd0336d26cf136e6abc4500262
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2025-06-24 12:10:08 +0200

    Update intruderAlert.lua

--- demos/intruderAlert.lua
+++ demos/intruderAlert.lua
@@ -1,10 +1,10 @@
-local mf = require("morefonts") ---requires https://pinestore.cc/projects/37/more-fonts
-local nbsTunes = require("nbsTunes") -- requires https://pinestore.cc/projects/18/nbs-tunes
+local mf = require(".morefonts") ---requires https://pinestore.cc/projects/37/more-fonts
+local nbsTunes = require(".nbsTunes") -- requires https://pinestore.cc/projects/18/nbs-tunes
 
 local sound = nbsTunes.load("tunes/redAlarm.nbs")
 
 ---Define monitor, clear it and define text color
-mon = peripheral.wrap("monitor_4")
+mon = peripheral.wrap("monitor_0")
 sound:stop()
 mon.clear()
 mon.setTextColor(colors.red)
