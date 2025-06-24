77b1d12ab1692d4410e0ba97abd69254b0824824
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2025-06-24 12:32:16 +0200

    Update aetherWellcome.lua

--- demos/aetherWellcome.lua
+++ demos/aetherWellcome.lua
@@ -4,4 +4,4 @@ mon = peripheral.wrap("monitor_0")
 mon.clear()
 mon.setTextColor(colors.lightBlue)
 --Write text on Monitor
-mf.writeOn(mon, "Wellcome to Aether Industries HQ", nil, nil, {font = "./fonts/Silkscreen", textAlign = "center", condense = true})
+mf.writeOn(mon, "Wellcome to Aether Industries HQ", nil, nil, {font = ".fonts/Silkscreen", textAlign = "center", condense = true})
