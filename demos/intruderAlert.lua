9ee27c35a8f23b46c34ef3a206ce0196ad2b0994
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2025-06-24 12:26:10 +0200

    Update intruderAlert.lua

--- demos/intruderAlert.lua
+++ demos/intruderAlert.lua
@@ -1,7 +1,7 @@
-local mf = require("..morefonts") ---requires https://pinestore.cc/projects/37/more-fonts
-local nbsTunes = require("..nbsTunes") -- requires https://pinestore.cc/projects/18/nbs-tunes
+local mf = require(".morefonts") ---requires https://pinestore.cc/projects/37/more-fonts
+local nbsTunes = require(".nbsTunes") -- requires https://pinestore.cc/projects/18/nbs-tunes
 
-local sound = nbsTunes.load("../tunes/redAlarm.nbs")
+local sound = nbsTunes.load("./tunes/redAlarm.nbs")
 
 ---Define monitor, clear it and define text color
 mon = peripheral.wrap("monitor_0")
@@ -13,4 +13,4 @@ mon.setTextColor(colors.red)
 sound:play()
 
 ---Write text on Monitor
-mf.writeOn(mon,  "!!!INTRUDER DETECTED!!!", nil, nil, {font = "../fonts/Silkscreen", textAlign = "center", condense = true})
+mf.writeOn(mon,  "!!!INTRUDER DETECTED!!!", nil, nil, {font = "./fonts/Silkscreen", textAlign = "center", condense = true})
